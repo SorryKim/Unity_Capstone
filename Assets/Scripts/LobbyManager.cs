3a0ccdb631fd1888a655011353bed4e24f281892
Kim, Jaeseong <06zzkimzz06@gmail.com>
2023-09-11 23:10:22 +0900

    Lobby화면구현
    로비화면 기본 구축

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,18 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+
+    private string gameVersion = "1"; // 게임버젼
+
+    public Text connectionInfoText; // 네트워크 정보 텍스트
+    public Button startButton; // 시작버튼
+
+
+    
+}
